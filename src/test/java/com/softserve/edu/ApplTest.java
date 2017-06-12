2773559c90d853423c2c7a4f53fce4e924fd3fc0
softservedata <garasym@gmail.com>
2017-06-12 16:01:54 +0300

    Get Memory. Examples

--- src/test/java/com/softserve/edu/ApplTest.java
+++ src/test/java/com/softserve/edu/ApplTest.java
@@ -2,13 +2,16 @@ package com.softserve.edu;
 
 import org.testng.annotations.Test;
 
-public class ApplTest 
-{
+public class ApplTest {
 	@Test
-    public void testApp()
-    {
-		System.out.println("surefire.reports.directory = " 
-				+ System.getProperty("surefire.reports.directory"));
-        System.out.println("AppTest done");
-    }
+	public void testApp() {
+		System.out.println("surefire.reports.directory = " + System.getProperty("surefire.reports.directory"));
+		//
+		long usedMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+		System.out.println("\nUsed Memory = " + usedMemory / (1024.0 * 1024.0) + "Mb");
+		System.out.println("\ntotalMemory() = " + Runtime.getRuntime().totalMemory() / (1024.0 * 1024.0) + "Mb");
+		System.out.println("maxMemory() = " + Runtime.getRuntime().maxMemory() / (1024.0 * 1024.0) + "Mb");
+		//
+		System.out.println("AppTest done");
+	}
 }
